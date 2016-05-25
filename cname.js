d664ded138b581a3c47e9286d77c0a81f008822f
Xinyu Zhang <xyzhdev@gmail.com>
2016-05-25 20:56:29 +0800

    add a CNAME help icon
    add a help icon (an “i” in a circle) in the right of CNAME text, and
    when it is clicked, the page will jump to Github document.

--- cname.js
+++ cname.js
@@ -17,8 +17,16 @@ module.exports = {
 
     if (template == 'settings') {
       // settings page
+
+      // include font-awesome
+      context.head =
+        `<link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">`;
+
       context.settings = `<div class="input-group">
-          <h5>CNAME</h5>
+          <h5>
+            CNAME
+            <a class="fa fa-exclamation-circle" href="https://help.github.com/articles/using-a-custom-domain-with-github-pages/" title="see Github pages custom CNAME document"></a>
+          </h5>
           <p><input type="text" name="plugin.cname" placeholder="your.blog (without http:// or https://)" value="${cname}"></p>
         </div>`;
       // All <input name='plugin.xxx'> will write the data into DB automatically
